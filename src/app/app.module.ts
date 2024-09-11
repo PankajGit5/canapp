5ba5c76a29312f24c301dcf1c0d10ea1c687d970
pankaj <pankaj@qpxtra.com>
2024-09-11 14:06:45 +0530

    Angular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { BannerComponent } from './banner/banner.component';
+import { TestComponent } from './test/test.component';
+import { ScoresComponent } from './scores/scores.component';
+import { DetailsComponent } from './details/details.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { DownloadsComponent } from './downloads/downloads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccordionComponent,
+    BannerComponent,
+    TestComponent,
+    ScoresComponent,
+    DetailsComponent,
+    CarouselComponent,
+    DownloadsComponent
   ],
   imports: [
     BrowserModule
